ecc28ff719a1f02de1315ea05654e61b12668221
Gabriel Camargo <gabrielcamargom07@gmail.com>
2021-04-24 03:13:49 -0300

    Primeiro protótipo; inimigos vindo pro jogador

---
+++ Main.gd
@@ -0,0 +1,39 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+func _ready():
+	randomize()
+	#new_game()
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_StartTimer_timeout():
+	$ScoreTimer.start()
+	$MobTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.offset = randi()
+	var mob = Mob.instance()
+	add_child(mob)
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	mob.position = $MobPath/MobSpawnLocation.position
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
